8884e0fc2ca1d4a172c9c8f4f15eff32ad80f718
Ephraim Pei <ephraimpei@gmail.com>
2016-02-03 16:32:59 -0500

    added Session model

---
+++ app/api/models/session.py
@@ -0,0 +1,33 @@
+from app import db
+import uuid
+
+class Session(db.Document):
+    username = db.StringField(max_length=255, required=True)
+    session_token = db.StringField(max_length=255, required=True)
+
+    @classmethod
+    def generate_session_token(cls):
+        return str(uuid.uuid1())
+
+    @classmethod
+    def find_by_session_token(cls, session_token):
+        return User.objects(session_token = session_token)
+
+    @classmethod
+    def destroy(cls, user):
+        if user.delete():
+            return True
+        else:
+            return False
+
+    def reset_session_token(self):
+        self.session_token = Session.generate_session_token()
+        self.save()
+        return self.session_token
+
+    def __repr__(self):
+        return '<session {0}><username {1}>'.format(self.session_token, self.username)
+
+    meta = {
+        'indexes': ['session_token']
+    }
